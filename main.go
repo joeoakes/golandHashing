b97d8590ce1520d466a207b5ef2ce375dfe25110
joeoakes <jxo19@psu.edu>
2023-12-03 11:31:38 -0500

    commit

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"crypto/md5"
 	"crypto/sha1"
+	"crypto/sha256"
 	"fmt"
 	"io"
 )
@@ -15,4 +16,11 @@ func main() {
 	h = sha1.New()
 	io.WriteString(h, "This class rocks")
 	fmt.Printf("% x", h.Sum(nil))
+
+	s := "{'payload':'hashLab'}"
+	h = sha256.New()
+	h.Write([]byte(s))
+	bs := h.Sum(nil)
+	fmt.Println(s)
+	fmt.Printf("%x\n", bs)
 }
